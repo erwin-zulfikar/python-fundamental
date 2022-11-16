be6cdc9f09aa8f9140720cc6eb3afd157b3c2a56
erwinzulfikar <erwin_zulfikar@yahoo.com>
2022-11-16 11:30:27 +0700

    perulangan dengan while dengan jumlah tak terbatas dan ada variabel pemberhenti

---
+++ dasar_sintaksis_perulangan_dengan_while_undefine_count.py
@@ -0,0 +1,25 @@
+"""
+Program perulangan membaca buku dengan while sampai paham
+"""
+buku = 10
+print('Ibu berkata, "Baca semua bukumu"')
+jumlah_baca = 0
+
+jumlah_paham = 0
+print(f"Jumlah buku yang sudah dibaca {jumlah_paham}")
+
+while jumlah_baca < buku * 2:
+    jumlah_baca = jumlah_baca + 1
+    if jumlah_paham == 9:
+        print(f"Buku ke {jumlah_paham + 1} belum paham")
+    else:
+        jumlah_paham = jumlah_paham + 1
+        print(f"Buku ke {jumlah_paham} sudah dibaca dan dipahami")
+
+print(f"Jumlah buku yang sudah dibaca dan dipahami {jumlah_paham}")
+
+if jumlah_paham == buku:
+    print('"Bu, semua buku sudah dibaca dan dipahami"')
+else:
+    print(f'"Bu, tidak semua buku bisa dipahami. Saya hanya bisa memahami {jumlah_paham} buku"')
+
